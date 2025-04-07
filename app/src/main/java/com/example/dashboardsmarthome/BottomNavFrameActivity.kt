f9283597f49625e6272d8f3e3854cfb0b7515821
Harits <muhammadharitsdetyairawan2004@mail.ugm.ac.id>
2025-04-07 16:33:57 +0700

    progress main_activity

---
+++ app/src/main/java/com/example/dashboardsmarthome/BottomNavFrameActivity.kt
@@ -0,0 +1,30 @@
+package com.example.dashboardsmarthome
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.dashboardsmarthome.databinding.BottomNavFrameBinding
+
+class BottomNavFrameActivity : AppCompatActivity() {
+
+private lateinit var binding: BottomNavFrameBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = BottomNavFrameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_bottom_nav_frame)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
