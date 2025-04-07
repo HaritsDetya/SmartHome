38501f35d983dececfcf6b7832028327c50f94ba
Syauqi <ahmadsyauqitaqiyuddin2004@mail.ugm.ac.id>
2025-04-07 16:34:16 +0700

    Add: font resource, energy detail page

---
+++ app/src/main/java/com/example/dashboardsmarthome/EnergyMonitoringActivity.kt
@@ -0,0 +1,20 @@
+package com.example.dashboardsmarthome
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EnergyMonitoringActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_energy_monitoring)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
